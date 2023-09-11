497a6dced901303a07aac0cfda38f68f6024e0ac
Nehoxa <loirejulien@outlook.fr>
2023-09-11 13:47:17 +0200

    Fix CORS

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'mediaapp/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://jloire.fr'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
